dca5368481d0e86c8f8cbc050c9c3f784f4cc470
Ajeet <ajeet.sharma@capgemini.com>
2018-01-15 16:53:01 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+// Express framework
+var express = require('express'); 
+var app = express();
+
+// Handle requests to /
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+// Setup port
+app.listen(process.env.PORT || 8000);
+
+module.exports = app;
