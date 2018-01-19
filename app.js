6ec0c65f83f36884a380b3208237ed8e4ca56ca3
Ajeet <ajeet.sharma@capgemini.com>
2018-01-19 16:19:08 +0530

    ssss

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var http = require('http');
 // Configure our HTTP server to respond with Hello World to all requests.
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello World Node JS");
+  response.end("Hello World Node JS. Running with Jenkins file");
 });
  
 // Listen on port 8000, IP defaults to "0.0.0.0"
